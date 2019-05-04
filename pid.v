3a8668c343973c75b9f836df1a27c2d855acf8f6
Andrew Nolte <anolte512@gmail.com>
2019-05-04 17:45:28 -0500

    did I and D

--- pid.v
+++ pid.v
@@ -3,15 +3,21 @@
 //motor outputs between 0 and 1024
 module pid(input clk, input signed[15:0] p, input signed[15:0] i, input signed [15:0] d, input[15:0] diff, output signed[15:0] net);
 
-
+    parameter lookbackLength = 3
     parameter decVals = 10;//the number of vals to the right of the decimal
     //need to do floating point. sensval and target can both be signed, or both unsigned
     //p is in terms of 500 * dps
     assign net = (diff * p) >>> decVals;
 
-    reg[63:0] accumulator;
-    reg[]
+    wire pterm = (diff * p) >>> decVals;
+    wire iterm = (accumulator * i) >>> decVals;
+    wire dterm = ((diff - prevValues[lastPtr+1]) * d) >>> decVals;
 
+    reg[63:0] accumulator = 0;
+    reg[15:0] prevValues [3:0] = 0;
+    reg [1:0] lastPtr = 0;
+    
+    //1 2 3 4
 
     always @(posedge clk) begin
         // down
@@ -21,8 +27,13 @@ module pid(input clk, input signed[15:0] p, input signed[15:0] i, input signed [
         // down 
         // up 3
 
+        //I stuff
+        accumulator <= accumulator + diff;
 
+        //D stuff
+        prevValues[lastPtr] = diff;
 
+        lastPtr <= lastPtr + 1;
     end
 
     //hopefully we don't have to do i and d :)
